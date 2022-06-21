c4b79aa96e777292f6538e5f95dfd5412cfc87f2
Nkunja <munene01denis@gmail.com>
2022-06-21 21:00:16 +0300

    seccond commit

---
+++ Assets/main.js
@@ -0,0 +1,6 @@
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+btn.addEventListener("click", function () {
+  alert("Hello"+"Sir Nkunja"+"Welcome to the Bootcamp");
+});
+document.body.appendChild(btn);
